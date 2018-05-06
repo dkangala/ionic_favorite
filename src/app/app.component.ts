b93e6ef70c20086add858f85909d815b946aef11
Kangala Shanmugham <dshanmugham@galaxy.lan>
2018-05-06 09:10:08 +0530

    Added tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,19 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { FavoritesPage } from '../pages/favorites/favorites';
+import { LibraryPage } from '../pages/library/library';
+import { QuotesPage } from '../pages/quotes/quotes';
+import { QuotePage } from '../pages/quote/quote';
+import { SettingsPage } from '../pages/settings/settings';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -19,4 +26,3 @@ export class MyApp {
     });
   }
 }
-
